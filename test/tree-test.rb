e1d66963d35011e8f8476f09fa0aeddd17368e76
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-03 11:34:31 -0600

    add a test for verifying if a node is included in a tree

--- test/tree-test.rb
+++ test/tree-test.rb
@@ -2,11 +2,26 @@ require 'minitest/autorun'
 require './lib/tree'
 
 class TreeTest < Minitest::Test
+  def setup
+    @tree = Tree.new
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(92, "Sharknado 3")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+  end
+
   def test_tree_inserts_nodes
-    tree = Tree.new
-    assert_equal(0, tree.insert(61, "Bill & Ted's Excellent Adventure"))
-    assert_equal(1, tree.insert(16, "Johnny English"))
-    assert_equal(1, tree.insert(92, "Sharknado 3"))
-    assert_equal(2, tree.insert(50, "Hannibal Buress: Animal Furnace"))
+    new_tree = Tree.new
+    assert_equal(0, new_tree.insert(61, "Bill & Ted's Excellent Adventure"))
+    assert_equal(1, new_tree.insert(16, "Johnny English"))
+    assert_equal(1, new_tree.insert(92, "Sharknado 3"))
+    assert_equal(2, new_tree.insert(50, "Hannibal Buress: Animal Furnace"))
+  end
+
+  def test_tree_includes_node
+    assert @tree.include?(61)
+    assert @tree.include?(16)
+    assert @tree.include?(50)
+    refute @tree.include?(42)
   end
 end
